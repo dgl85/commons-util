0b764ba4e72c65c100c89215b9a683625abbf2e5
Domingo Guzman <domingoxguzman@gmail.com>
2019-02-01 11:33:06 +0100

    ShiftlessArrayListSubView

---
+++ src/main/java/org/dgl/commons/util/ShiftlessArrayListSubView.java
@@ -0,0 +1,34 @@
+package org.dgl.commons.util;
+
+public class ShiftlessArrayListSubView<T> {
+
+    private final ShiftlessArrayList<T> parent;
+    private final int startIndex;
+    private final int endIndex;
+    private final int size;
+
+    /**
+     *
+     * @param parent
+     * @param startIndex inclusive
+     * @param endIndex exclusive
+     */
+    public ShiftlessArrayListSubView(ShiftlessArrayList<T> parent, int startIndex, int endIndex) {
+        this.parent = parent;
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+        size = endIndex - startIndex;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return parent.get(startIndex+index);
+    }
+
+}
